c2a6fcda888271240cc7e910966c2c7504ca5b2a
osakac <shinmoriof@gmail.com>
2024-09-13 14:19:43 +0400

    добавление хедера

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <TheHeader />
 </template>
 
+<script setup>
+import TheHeader from "./components/Header/TheHeader.vue";
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import "./assets/styles/normalize.css";
+@import "./assets/styles/reset.css";
+@import "./assets/styles/index.css";
 </style>
---
+++ src/components/Header/TheHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header :class="css.headerWrapper">
+    <div class="container" :class="css.header">
+      <img src="../../assets/svg/logo.svg" alt="Alef development logo" />
+
+      <nav>
+        <ul :class="css.list">
+          <li>
+            <router-link to="/form">Форма</router-link>
+          </li>
+          <li>
+            <router-link to="/preview">Превью</router-link>
+          </li>
+        </ul>
+      </nav>
+
+      <div></div>
+    </div>
+  </header>
+</template>
+
+<script setup>
+import css from "./TheHeader.module.css";
+</script>
